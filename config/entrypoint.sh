5392e0ec9a0287d8c56053fc19215e49085767f5
Tiago M Reichert <tiago@reichert.eti.br>
2019-04-08 14:42:50 -0300

    Added From Address

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -11,6 +11,7 @@ sed -ri "s|<ES_PASSWORD>|$ES_PASSWORD|g" /opt/elastalert/config.yaml
 sed -ri "s|<ES_VERIFY_CERT>|$ES_VERIFY_CERT|g" /opt/elastalert/config.yaml
 sed -ri "s|<SMTP_HOST>|$SMTP_HOST|g" /opt/elastalert/config.yaml
 sed -ri "s|<SMTP_PORT>|$SMTP_PORT|g" /opt/elastalert/config.yaml
+sed -ri "s|<FROM_ADDRESS>|$FROM_ADDRESS|g" /opt/elastalert/config.yaml
 sed -ri "s|<SMTP_USERNAME>|$SMTP_USERNAME|g" /opt/elastalert/mail_auth.yml
 sed -ri "s|<SMTP_PASSWORD>|$SMTP_PASSWORD|g" /opt/elastalert/mail_auth.yml
 
